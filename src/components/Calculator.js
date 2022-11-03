ab204c8af65a859533536a3469b2552671e4fb47
Doheera-kosi <kupourevans99@gmail.com>
2022-11-03 14:00:41 +0000

    Refactored Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,70 +1,51 @@
-import React, { Component } from 'react';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
 import Btn from './Button';
 import calculate from '../dialogue/calculator';
 
-class CalcRender extends Component {
-  constructor(props) {
-    super(props);
-    this.Val = [
-      { btn: 'AC', style: 'btn gc' },
-      { btn: '+/-', style: 'btn gc' },
-      { btn: '%', style: 'btn gc' },
-      { btn: '÷', style: 'btn orange' },
-      { btn: '7', style: 'btn gc' },
-      { btn: '8', style: 'btn gc' },
-      { btn: '9', style: 'btn gc' },
-      { btn: 'x', style: 'btn orange' },
-      { btn: '4', style: 'btn gc' },
-      { btn: '5', style: 'btn gc' },
-      { btn: '6', style: 'btn gc' },
-      { btn: '-', style: 'btn orange' },
-      { btn: '1', style: 'btn gc' },
-      { btn: '2', style: 'btn gc' },
-      { btn: '3', style: 'btn gc' },
-      { btn: '+', style: 'btn orange' },
-      { btn: '0', style: 'btn gc btn-zero' },
-      { btn: '.', style: 'btn gc' },
-      { btn: '=', style: 'btn orange' },
-    ];
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
-
-  handleClick = (e) => {
-    const obj = calculate(this.state, e);
-    this.setState(obj);
-  }
-
-  render() {
-    let { total, next, operation } = this.state;
-    if (!total && !next && !operation) {
-      total = 0;
-      next = null;
-      operation = null;
+function calculator() {
+  const [result, setResult] = useState({ total: 0, next: null, operation: null });
+  const Val = [
+    { btn: 'AC', style: 'btn gc' },
+    { btn: '+/-', style: 'btn gc' },
+    { btn: '%', style: 'btn gc' },
+    { btn: '÷', style: 'btn orange' },
+    { btn: '7', style: 'btn gc' },
+    { btn: '8', style: 'btn gc' },
+    { btn: '9', style: 'btn gc' },
+    { btn: 'x', style: 'btn orange' },
+    { btn: '4', style: 'btn gc' },
+    { btn: '5', style: 'btn gc' },
+    { btn: '6', style: 'btn gc' },
+    { btn: '-', style: 'btn orange' },
+    { btn: '1', style: 'btn gc' },
+    { btn: '2', style: 'btn gc' },
+    { btn: '3', style: 'btn gc' },
+    { btn: '+', style: 'btn orange' },
+    { btn: '0', style: 'btn gc btn-zero' },
+    { btn: '.', style: 'btn gc' },
+    { btn: '=', style: 'btn orange' },
+  ];
+  const handleClick = (e) => {
+    const obj = calculate(result, e);
+    if (!obj.total && !obj.next && !obj.operation) {
+      obj.total = 0;
+      obj.next = null;
+      obj.operation = null;
     }
-    return (
-      <div className="calc-container">
-        <div className="calc-output">
-          { next || operation || total }
-        </div>
-        <div className="btns-container">
-          {
-            this.Val.map((b) => (
-              <Btn
-                key={b.btn}
-                btn={b.btn}
-                event={(e) => this.handleClick(e)}
-                style={b.style}
-              />
-            ))
-          }
-        </div>
+    setResult(obj);
+  };
+  return (
+    <div className="calc-container">
+      <div className="calc-output">
+        { result.next || result.operation || result.total }
       </div>
-    );
-  }
+      <div className="btns-container">
+        {
+          Val.map((b) => <Btn key={b.btn} btn={b.btn} event={(e) => handleClick(e)} style={b.style} />)
+        }
+      </div>
+    </div>
+  );
 }
-
-export default CalcRender;
+export default calculator;
