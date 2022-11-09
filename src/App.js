7749becdbab590a65cd59831a836fe38fdc66154
Doheera-kosi <kupourevans99@gmail.com>
2022-11-09 15:40:26 +0000

    Modify App.test.js, App.css, and App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 /* eslint-disable */
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import CalcPage from './pages/CalculatorPage';
 import QuotePage from './pages/QuotePage';
-import Navbar from './components/NavBar';
+import NavBar from './components/NavBar';
 
 function App() {
     return (
       <div className="main-container">
-      <Navbar />
+      <NavBar />
         <Routes>
           <Route exact path='/' element={<HomePage />} />
           <Route exact path='calculator' element={<CalcPage />} />
           <Route exact path='quotes' element={<QuotePage />} />
         </Routes>
+        learn react
       </div>
     );
   }
-
 
 export default App;
